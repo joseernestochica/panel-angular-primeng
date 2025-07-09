e262353246d37f6f631691f3a821f6194afd0676
Ernesto Chica <joseernestochica@gmail.com>
2025-07-09 12:38:59 +0200

    Actualización de configuraciones y estilos: se incrementó el límite de advertencia en angular.json, se cambió el objetivo de TypeScript a ES2022, se actualizó el tema en index.html y app.layout.service.ts, y se ajustó la importación de estilos en styles.scss para PrimeNG 18.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { PrimeNGConfig, Translation } from 'primeng/api';
 
 
 @Component( {
@@ -8,20 +7,10 @@ import { PrimeNGConfig, Translation } from 'primeng/api';
 } )
 export class AppComponent implements OnInit {
 
-    constructor ( private primengConfig: PrimeNGConfig ) { }
+    constructor () { }
 
     ngOnInit () {
-        this.primengConfig.ripple = true;
-        this.primengConfig.setTranslation( {
-            clear: 'Limpiar',
-            apply: 'Aplicar',
-            dayNames: [ "Domingo", "Lunes", "Martes", "Miercoles", "Jueves", "Viernes", "Sábado" ],
-            dayNamesShort: [ "Do", "Lu", "Ma", "Mi", "Ju", "Vi", "Sa" ],
-            dayNamesMin: [ "Do", "Lu", "Ma", "Mi", "Ju", "Vi", "Sa" ],
-            monthNames: [ "Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre" ],
-            monthNamesShort: [ "Ene", "Feb", "Mar", "Abr", "May", "Jun", "Jul", "Ago", "Sep", "Oct", "Nov", "Dic" ],
-            dateFormat: "dd/mm/yy",
-            "firstDayOfWeek": 1,
-        } );
+        // PrimeNG 18 no requiere configuración global
+        // Las traducciones se manejan a nivel de componente
     }
 }
